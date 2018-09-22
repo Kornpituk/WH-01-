67a5bbdd4340f50245526adf5dedaf14dd850e93
Kornpituk <43356993+Kornpituk@users.noreply.github.com>
2018-09-22 13:59:00 +0700

    Code T,C,S

---
+++ WH01.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	float hight,base,radius,side;
+	float AswT,AswC,AswS;
+	
+	printf("-------Triangle---------\n");
+	printf("Triangle Area Calculator\n");
+	printf("Enter Hight :");
+	scanf("%f",&hight);
+	printf("Enter Base :");
+	scanf("%f",&base);
+	AswT=(hight*base)*1/2;
+	printf("Area of Triangle = %.2f\n",AswT);
+	
+	printf("-------Circle---------\n");
+	printf("Triangle Area Calculator\n");
+	printf("Enter Radius :");
+	scanf("%f",&radius);
+	AswC = (radius*radius)*M_PI;
+	printf("Area of Triangle = %.2f\n",AswC);
+	
+	printf("-------Square---------\n");
+	printf("Triangle Area Calculator\n");
+	printf("Enter Side :");
+	scanf("%f",&side);
+	AswS = side*side;
+	printf("Area of Triangle = %.2f\n",AswS);
+	 
+	 //
+	return 0;
+}
